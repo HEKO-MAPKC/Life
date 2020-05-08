2a44a6bd8d798bb1c9c10468fc20ac4e876a4e20
Michael <michaelkim@ya.ru>
2020-05-09 02:56:46 +0700

    Add phares for Goddess and new emotions

--- Goddess_bless.cpp
+++ Goddess_bless.cpp
@@ -169,6 +169,9 @@ public:
 	void sprite_draw(RenderWindow &window) {
 		window.draw(sprite_t);
 	}
+	void set_pos(int pos_x, int pos_y) {
+		sprite_t.setPosition(pos_x, pos_y);
+	}
 };
 
 class text_class {
@@ -206,7 +209,7 @@ void init_game() {
 	int delay_t = 0;
 	int startX = 700, startY = 50, tilesize = 8;
 	int pos_j, pos_i;
-	int num_scene = 1;
+	int num_scene = 2;
 	float CurrentFrame = 0;
 	string* coord_world;
 	coord_world = new string[coord_num];
@@ -231,14 +234,15 @@ void init_game() {
 	music.play();
 	music.setLoop(true);
 	music.setVolume(10.f);
-	Texture Goddess_t;
-	Goddess_t.loadFromFile("nep.png");
-	sprite_class fon,goddess,planet,box,day_box;
+	sprite_class fon,goddess,planet,box,day_box,yan;
 	fon.init_sprite(0,0,"fon.png");
 	planet.init_sprite(startX - 182, startY - 185, "planet.png");
 	box.init_sprite(10, 410, "box.png");
 	day_box.init_sprite(0, 0, "day.png");
-	goddess.init_sprite(522, 0, 522, 800, 0, 100, 1, "nep.png");
+	goddess.init_sprite(522, 0, 522, 919, 0, -19, 1, "nep.png");
+	//////////////////
+	goddess.set_pos(0, -19);
+	/////////////////
 	Font font;
 	font.loadFromFile("font.ttf");
 	text_class text_box[4],textsp,day_num_text,num_text;
@@ -249,7 +253,6 @@ void init_game() {
 	day_num_text.init_text(L"", 32, font, 135, 32);
 	num_text.init_text(L"", 32, font, 35, 32);
 	RenderWindow window(VideoMode(1600, 900), "Goddess bless this PC");
-	set_text_arr(L"Gehaburn... Оружие,", L"забравшее жизнь всех", L"кого я любила теперь единственное", L"что осталось от прошлой меня.", window, text_box);
 	window.clear(Color::White);
 	RectangleShape rectangle(Vector2f(tilesize, tilesize));
 	RectangleShape rectangle_life(Vector2f(tilesize, tilesize));
@@ -271,19 +274,19 @@ void init_game() {
 	}
 	while (window.isOpen()) {
 		switch (num_scene) {
+		Event event;
 		case 1:
-			Event event;
 			while (window.pollEvent(event)) {
 				if (event.type == Event::Closed)
 					window.close();
 				if (event.type == Event::MouseButtonPressed) {
-					if (Mouse::getPosition(window).x >= startX && sf::Mouse::getPosition(window).y >= startY && sf::Mouse::getPosition(window).y <= ((n - 1) * tilesize + startY) && sf::Mouse::getPosition(window).x <= ((n - 1) * tilesize + startX))
+					if (Mouse::getPosition(window).x >= startX && sf::Mouse::getPosition(window).y >= startY && sf::Mouse::getPosition(window).y <= ((n) * tilesize + startY) && sf::Mouse::getPosition(window).x <= ((n) * tilesize + startX))
 						if (event.key.code == Mouse::Left) {
 							isMove = true;
 						}
 				}
 				if (event.type == Event::MouseButtonPressed) {
-					if (Mouse::getPosition(window).x >= startX && sf::Mouse::getPosition(window).y >= startY && sf::Mouse::getPosition(window).y <= ((n - 1) * tilesize + startY) && sf::Mouse::getPosition(window).x <= ((n - 1) * tilesize + startX))
+					if (Mouse::getPosition(window).x >= startX && sf::Mouse::getPosition(window).y >= startY && sf::Mouse::getPosition(window).y <= ((n) * tilesize + startY) && sf::Mouse::getPosition(window).x <= ((n) * tilesize + startX))
 						if (event.key.code == Mouse::Right) {
 							Vector2i pixelPos = Mouse::getPosition(window);
 							pos_j = (pixelPos.x - startX) / tilesize;
@@ -296,13 +299,15 @@ void init_game() {
 						isMove = false;
 				if (isMove)
 				{
-					Vector2i pixelPos = Mouse::getPosition(window);
-					pos_j = (pixelPos.x - startX) / tilesize;
-					pos_i = (pixelPos.y - startY) / tilesize;
-					world[pos_i][pos_j] = 1;
-					goddess.set_anim_frame(522, 0, 522, 800, 0);
-					day_num = 0;
-					cout << "Choosing" << endl;
+						Vector2i pixelPos = Mouse::getPosition(window);
+						if (Mouse::getPosition(window).x >= startX && sf::Mouse::getPosition(window).y >= startY && sf::Mouse::getPosition(window).y <= ((n) * tilesize + startY) && sf::Mouse::getPosition(window).x <= ((n) * tilesize + startX)) {
+							pos_j = (pixelPos.x - startX) / tilesize;
+							pos_i = (pixelPos.y - startY) / tilesize;
+							world[pos_i][pos_j] = 1;
+							goddess.set_anim_frame(522, 0, 522, 919, 0);
+							set_text_arr(L"Жизнь это чудесно,", L"не так ли?^^", L"", L"", window, text_box);
+							day_num = 0;
+						}
 				}
 				if (event.type == Event:: KeyPressed) {
 					if (event.key.code == Keyboard::S) {
@@ -310,17 +315,24 @@ void init_game() {
 					}
 					if (event.key.code == Keyboard::Enter) {
 						if (!world_started) world_started = 1;
-						goddess.set_anim_frame(522, 0, 522, 800, 0);
-						cout << "Key pressed" << endl;
+						goddess.set_anim_frame(522, 0, 522, 919, 0);
 					}
 					if (event.key.code == Keyboard::R) {
+						goddess.set_anim_frame(522, 0, 700, 919, 4);
+						set_text_arr(L"Уничтожить всю жизнь?", L"Как жестоко...", L"", L"", window, text_box);
 						null_mas(world, n);
 						day_num = 0;
+						world_started = 0;
 					}
 					if (event.key.code == Keyboard::Space) {
 						rand_mas(world, n);
 						day_num = 0;
-						cout << "Key pressed SPACE" << endl;
+					}
+					if (event.key.code == Keyboard::Q) {
+						for (int i = 0; i < n; i++) {
+							world[n / 2][i] = 1;
+						}
+						day_num = 0;
 					}
 				}
 			}
@@ -344,16 +356,22 @@ void init_game() {
 			if (world_started) {
 				day(world, world_after, n);
 				if (day_num <= 30) {
-					goddess.set_anim_frame(522, 0, 522, 800, 0);
+					goddess.set_anim_frame(522, 0, 522, 919, 0);
+					set_text_arr(L"Похоже твоя жизнь делает", L"успехи!~", L"", L"", window, text_box);
 				}
 				if (day_num > 100) {
-					goddess.set_anim_frame(522, 0, 522, 800, 2);
+					goddess.set_anim_frame(522, 0, 522, 919, 2);
+					set_text_arr(L"Сугой, десу (>w<)", L"", L"", L"", window, text_box);
 				}
 				day_num++;
 				get_coordinates(world_after, coord_world[day_num % (coord_num)], n);
 				if (compare_coordinates(coord_world, coord_num)) {
 					cout << "Compare_coord" << endl;
-					goddess.set_anim_frame(522, 0, 522, 800, 3);
+					goddess.set_anim_frame(522, 0, 522, 919, 3);
+					if (check_null_world(world, n))
+						set_text_arr(L"Похоже ты подарил миру", L"слишком мало жизни...", L"", L"", window, text_box);
+					else 
+						set_text_arr(L"Стагнация мира то же", L"что и смерть...", L"", L"", window, text_box);
 					day_num = 0;
 					world_started = 0;
 					null_compare_coord(coord_world, coord_num);
@@ -372,12 +390,33 @@ void init_game() {
 			day_box.sprite_draw(window);
 			num_text.set_text(L"Дней: ", window);
 			day_num_text.set_text(to_wstring(day_num), window);
-			textsp.set_text(L"Богиня", window);
-			set_text_arr(L"Gehaburn... Оружие,", L"забравшее жизнь всех", L"кого я любила теперь единственное", L"что осталось от прошлой меня.", window, text_box);
+			for (int i = 0; i < 4; i++) {
+				text_box[i].text_draw(window);
+			}
+			textsp.set_text(L"Богиня", window); 
 			window.display();
 		break;
 		case 2:
-
+			while (window.pollEvent(event)) {
+				if (event.type == Event::Closed)
+					window.close();
+			}
+			fon.sprite_draw(window);
+			planet.sprite_draw(window);
+			goddess.sprite_draw(window);
+			box.sprite_draw(window);
+			textsp.set_text(L"Богиня", window);
+			set_text_arr(L"Приветствую, Анон.", L"Я Богиня ЭВМ", L"и я благославляю этот ПК", L"", window, text_box);
+			window.display();
+			Sleep(3000);
+			fon.sprite_draw(window);
+			planet.sprite_draw(window);
+			goddess.sprite_draw(window);
+			box.sprite_draw(window);
+			textsp.set_text(L"Богиня", window);
+			set_text_arr(L"А этот мир я дарю тебе.", L"Подари же ему жизни!", L"", L"", window, text_box);
+			num_scene = 1;
+			window.display();
 		break;
 		}
 	}
